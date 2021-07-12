20c4188956f9b430607bdeaa331867a37469f53f
Mohammad <malnajjar16@gmail.com>
2021-07-12 13:35:00 +0200

    hackYourTemp-w2

---
+++ week2/homework/exercises/hackyourtemperature/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // middleware to handle the content-type application/x-www-form-urlencoded
+
+// setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// post city name
+app.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  console.log(req.body.cityName);
+
+  //rendering the weather page
+  res.status(200).render("weather", { cityName });
+});
+app.listen(3000, () => console.log("server started ..."));
